fbebe7331295ff19fb20c6f843c6d0a593fa0f69
nnarziev95 <nnarziev@gmail.com>
2018-06-12 19:33:38 +0900

    Modifications done

--- app/src/main/java/kr/ac/inha/nsl/mindnavigator/FeedbackActivity.java
+++ app/src/main/java/kr/ac/inha/nsl/mindnavigator/FeedbackActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.SeekBar;
